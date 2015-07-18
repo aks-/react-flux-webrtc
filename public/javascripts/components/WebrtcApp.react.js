fc6094804119bee6223d29820f080aa0b8afeef9
aks- <coderatlabs@gmail.com>
2015-07-18 20:09:40 +0530

    Add main WebrtcApp component [WIP]

---
+++ public/javascripts/components/WebrtcApp.react.js
@@ -0,0 +1,19 @@
+var React = require('react');
+var VideoSection = require('./VideoSection.react');
+var AudioSection = require('./AudioSection.react');
+var CallButtons = require('./CallButtons.react');
+
+var WebrtcApp = React.createClass({
+
+  render: function() {
+    return (
+      <div className="container">
+        <div id="error-message"></div>
+        <VideoSection />
+        <AudioSection />
+        <CallButtons />
+      </div>
+    );
+  }
+
+});
