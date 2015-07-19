9544f35af89c6f036ba13b2d64631c5cbe7bc5b6
aks- <coderatlabs@gmail.com>
2015-07-19 20:44:34 +0530

    REFACTOR!!!!!!

--- public/javascripts/utils/WebrtcAPI.js
+++ public/javascripts/utils/WebrtcAPI.js
@@ -1,87 +1,84 @@
 var bluebird = require('bluebird');
 var WebrtcAppDispatcher = require('../dispatcher/WebrtcAppDispatcher');
-var videoSupport = false;
-var AudioSupport = false;
 var serverConfig = null;
 var localPeerConnection, remotePeerConnection;
 
 var WebrtcAPI = {
-  audioCall: function() {
+  makeAudioCall: function() {
+    var constraints = {audio: true};
+    this._call(constraints, MAKE_AUDIO_CALL);
+  },
+  makeVideoCall: function() {
+    var constraints = {video: true, audio: true};
+    this._call(constraints, MAKE_VIDEO_CALL);
+  },
+  hangup: function() {
+    call.hang();
+  },
+  _call: function(constraints, event) {
     bluebird.promisify(getUserMedia(constraints))
-    .then(function(stream) {
-      setAudioVideoSupport(stream);
-      return stream;
-    })
-    .then(function(stream) {
-      console.log('Received local stream');
-      localStream = stream;
-      return localStream;
-    })
+    .then(gotStream)
     .then(function(stream) {
       var objectUrl = URL.createObjectURL(stream);
       //TODO dispatch it to components to change the view
       return stream;
     })
-    .then(function(stream) {
-      localPeerConnection = new RTCPeerConnection(serverConfig);
-      localPeerConnection.onicecandidate = gotLocalIceCandidate;
-      remotePeerConnection = new RTCPeerConnection(serverConfig);
-      remotePeerConnection.onicecandidate = gotRemoteIceCandidate;
-      remotePeerConnection.onaddstream = gotRemoteStream;
-      localPeerConnection.addStream(stream);
-      localPeerConnection.createOffer(gotLocalDescription, handleError);
-    })
+    .then(createConnection)
     .fail(function() {
       //TODO create error-message component and change starte using dispatcher
     });
-
-    function gotRemoteStream(evt) {
-      var objectUrl = URL.createObjectURL(evt.stream);
-      //TODO dispatch it to component of remote video
-    }
-
-    function gotLocalDescription(description) {
-      localPeerConnection.setLocalDescription(description);
-      trace('Offer from localPeerConnection: \n', description.sdp);
-      remotePeerConnection.setRemoteDescription(description);
-      remotePeerConnection.createAnswer(gotRemoteDescription, handleError);
-    }
-
-    function gotRemoteDescription(description) {
-      remotePeerConnection.setLocalDescription(description);
-      trace('Answer from remotePeerConnection: \n', description.sdp);
-      localPeerConnection.setRemoteDescription(description);
-    }
-
-    function gotLocalIceCandidate(event) {
-      if (event.candidate) {
-        remotePeerConnection.addIceCandidate(new RTCIceCandidate(event.candidate));
-        trace('Local ICE candidate: \n', event.candidate.candidate);
-      }
-    }
-
-    function gotRemoteIceCandidate(event) {
-      if (event.candidate) {
-        localPeerConnection.addIceCandidate(new RTCIceCandidate(event.candidate));
-        trace('Remote ICE candidate: \n', event.candidate.candidate);
-      }
-    }
-
-    function handleError() {};
-  },
-  makeAudioCall: function() {
-    var audioConstraints = {audio: true};
-    peerConnection.init({localStream: localStream, gotStream: gotRemoteStream});
-    makeCall('audio', 'localAudio', 'remoteAudio', audioCallButton, audioConstraints, hasAudioSupport);
-  },
-  makeVideoCall: function() {
-    var videoConstraints = {video: true, audio: true};
-    makeCall('video', 'localVideo', 'remoteVideo', videoCallButton, videoConstraints, hasVideoSupport);
-  },
-  hangup: function() {
-    call.hang();
   }
 };
+
+function gotStream(stream) {
+  console.log('Received local stream');
+  localStream = stream;
+  return localStream;
+}
+
+function createConnection(stream) {
+  localPeerConnection = new RTCPeerConnection(serverConfig);
+  localPeerConnection.onicecandidate = gotLocalIceCandidate;
+  remotePeerConnection = new RTCPeerConnection(serverConfig);
+  remotePeerConnection.onicecandidate = gotRemoteIceCandidate;
+  remotePeerConnection.onaddstream = gotRemoteStream;
+  localPeerConnection.addStream(stream);
+  localPeerConnection.createOffer(gotLocalDescription, handleError);
+}
+
+function gotRemoteStream(evt) {
+  var objectUrl = URL.createObjectURL(evt.stream);
+  //TODO dispatch it to component of remote video
+}
+
+function gotLocalDescription(description) {
+  localPeerConnection.setLocalDescription(description);
+  trace('Offer from localPeerConnection: \n', description.sdp);
+  remotePeerConnection.setRemoteDescription(description);
+  remotePeerConnection.createAnswer(gotRemoteDescription, handleError);
+}
+
+function gotRemoteDescription(description) {
+  remotePeerConnection.setLocalDescription(description);
+  trace('Answer from remotePeerConnection: \n', description.sdp);
+  localPeerConnection.setRemoteDescription(description);
+}
+
+function gotLocalIceCandidate(event) {
+  if (event.candidate) {
+    remotePeerConnection.addIceCandidate(new RTCIceCandidate(event.candidate));
+    trace('Local ICE candidate: \n', event.candidate.candidate);
+  }
+}
+
+function gotRemoteIceCandidate(event) {
+  if (event.candidate) {
+    localPeerConnection.addIceCandidate(new RTCIceCandidate(event.candidate));
+    trace('Remote ICE candidate: \n', event.candidate.candidate);
+  }
+}
+
+function handleError() {};
 
 function setAudioVideoSupport(stream) {
   if (stream.getVideoTracks().length > 0) {
@@ -92,6 +89,7 @@ function setAudioVideoSupport(stream) {
     audioSupport = true;
     console.log('Using video device: ', stream.getAudioTracks()[0].label);
   }
+  return stream;
 }
 
 module.exports = WebrtcAPI;
